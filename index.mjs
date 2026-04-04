11a3ffc706bb2b47f55fed106243fba8b3946d70
Katz100 <cody.hopkins108@gmail.com>
2026-04-04 11:04:45 -0700

    initial

---
+++ index.mjs
@@ -0,0 +1,39 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//for Express to get values using POST method
+app.use(express.urlencoded({extended:true}));
+
+//setting up database connection pool
+const pool = mysql.createPool({
+    host: "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ofrrz451lt9y96o4",
+    password: "v71ailar76wlh8dm",
+    database: "it7vsj6f4a94t599",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+
+//routes
+app.get('/', (req, res) => {
+   res.send('Hello Express app!')
+});
+
+app.get("/dbTest", async(req, res) => {
+   try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error");
+    }
+});//dbTest
+
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
